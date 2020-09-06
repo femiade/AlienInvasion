6ff37385fd6fa92c43435f101560986e219cab1e
Femi Adebayo <femiadebayojr@gmail.com>
2020-09-06 14:41:19 -0400

    adding files to repo

---
+++ test.py
@@ -0,0 +1,40 @@
+juices = 4
+
+
+name = "femi"
+num = 5
+
+
+if num != 5:
+    print("")
+elif num == 6:
+    print("")
+else:
+    print("")
+
+
+messageForStudents = "Welcome"
+
+firstName = "Hawa"
+lastName = "Anthony"
+
+
+fullName = firstName.upper() + " " + lastName
+
+
+numDie = ['1', '2', '3', '4', '5', '6']
+
+
+numDieInt = [1, 2, 3, 4, 5, 6]
+
+
+randomList = [1, 2, 3, "femi", 4, 5, "femi"]
+
+
+#print("My name is " + randomList[-1])
+
+# for item in randomList:
+
+dict = {"name": "femi", "age": 25}
+
+print(dict["name"])
